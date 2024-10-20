1eece233e35576d20901889819f1ae2a2f2293a9
Omkar <sonawaneomkar2306@gmail.com>
2024-10-20 16:34:19 +0530

    Navbar and Sidebar Implemented

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Sidebar from "root/components/home/sidebar";
+import Navbar from "root/components/navbar";
 
 export default function Home() {
   return (
     <div>
-      hello
+      <Navbar/>
+      <Sidebar/>
     </div>
   );
 }
