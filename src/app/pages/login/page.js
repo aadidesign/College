509aa8097fc4d355048b779dd54fc1843f2d0921
Omkar <sonawaneomkar2306@gmail.com>
2024-10-20 20:10:05 +0530

    Build Ready

--- src/app/pages/login/page.js
+++ src/app/pages/login/page.js
@@ -88,7 +88,7 @@ export default function LoginPage() {
               </Link>
             </p>
             <p className="text-center">
-              Don't have an account?{" "}
+              Dont have an account?{" "}
               <Link href="/register" className="text-blue-600 hover:underline">
                 Sign Up
               </Link>
