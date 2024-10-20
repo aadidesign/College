8187a2dc109b2bf463448a47a75f2964fa60a4b9
Omkar <sonawaneomkar2306@gmail.com>
2024-10-20 18:24:22 +0530

    Sorted Everything

---
+++ src/app/pages/home/page.js
@@ -0,0 +1,17 @@
+// pages/index.js
+import React from 'react';
+import Navbar from '../../../../../components/navbar';
+import Sidebar from 'root/components/home/sidebar';
+import Hero from 'root/components/home/hero';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Sidebar/>
+      <Hero/>
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/app/pages/login/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LoginPage from 'root/components/auth/login'
+
+const page = () => {
+  return (
+    <div>
+        <LoginPage/>
+    </div>
+  )
+}
+
+export default page
