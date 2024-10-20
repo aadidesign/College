c54cee6b4b686e3cccd7b1013a466f94c92a0a91
Soham Hanse <120187322+sohamhanse@users.noreply.github.com>
2024-10-20 17:50:12 +0530

    login update

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,13 @@ import Image from "next/image";
 import Sidebar from "root/components/home/sidebar";
 import Navbar from "root/components/navbar";
 import Hero from "root/components/home/hero";
+import Login from "root/components/auth/login";
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
-      <Sidebar/>
-      <Hero/>
+      <Login/>
     </div>
   );
 }
