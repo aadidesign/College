d2579be5aa501169d65cdcf7c16bbc83f84148c5
Omkar <sonawaneomkar2306@gmail.com>
2024-10-20 17:22:04 +0530

    Home Page Completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Sidebar from "root/components/home/sidebar";
 import Navbar from "root/components/navbar";
+import Hero from "root/components/home/hero";
 
 export default function Home() {
   return (
     <div>
       <Navbar/>
       <Sidebar/>
+      <Hero/>
     </div>
   );
 }
