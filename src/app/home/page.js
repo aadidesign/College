90949332a16a76300dbbf3fe05cac473a4563b10
Omkar <sonawaneomkar2306@gmail.com>
2024-10-20 17:58:50 +0530

    Added Calender

---
+++ src/app/home/page.js
@@ -0,0 +1,13 @@
+// pages/index.js
+import React from 'react';
+import FullCalendar from '../../components/calender/calender';
+
+const HomePage = () => {
+  return (
+    <div>
+      <FullCalendar />
+    </div>
+  );
+};
+
+export default HomePage;
